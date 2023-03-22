d8af5713b9ea137ac3369f818a32287e32d0af89
Rawan <rawnq.sb@gmail.com>
2023-03-23 00:54:47 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default function EyesOnMe (){
+    return (
+        <button
+            onFocus={()=>{console.log("Good!")}}
+            onBlur={()=>{console.log("Hey! Eyes on me!")}}>
+            Eyes on me
+        </button>
+    )
+} 
